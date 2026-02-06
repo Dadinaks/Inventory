817f8b48cf6dec10d2e89b9abd886246821e68d0
Dadinaks <dadinakeli@gmail.com>
2026-02-06 18:12:16 +0300

    [Backend] : Update JWTInvalidationListener.php

--- backend/src/User/Infrastructure/Security/EventListener/JWTInvalidationListener.php
+++ backend/src/User/Infrastructure/Security/EventListener/JWTInvalidationListener.php
@@ -2,27 +2,48 @@
 
 namespace Dadinaks\User\Infrastructure\Security\EventListener;
 
+use Dadinaks\Shared\Domain\Repository\RepositoryInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
 use Psr\Cache\CacheItemPoolInterface;
 
 final class JWTInvalidationListener
 {
     public function __construct(
-        private readonly CacheItemPoolInterface $cache
+        private readonly CacheItemPoolInterface $cache,
+        private readonly RepositoryInterface $repository
     ) {}
 
     public function onJWTDecoded(JWTDecodedEvent $event): void
     {
         $payload = $event->getPayload();
 
-        if (!isset($payload['jti'])) {
+        if (isset($payload['jti'])) {
+            $cacheItem = $this->cache->getItem(key: 'jwt_blocklist_' . $payload['jti']);
+            if ($cacheItem->isHit()) {
+                $event->markAsInvalid();
+                return;
+            }
+        }
+
+        if (!isset($payload['username'])) {
+            $event->markAsInvalid();
             return;
         }
 
-        $cacheItem = $this->cache->getItem(key: 'jwt_blocklist_' . $payload['jti']);
+        $user = $this->repository->findOneBy(criteria: ['username' => $payload['username']]);
 
-        if ($cacheItem->isHit()) {
+        if (!$user->getIsConnected()) {
             $event->markAsInvalid();
+            return;
+        }
+
+        if (!$user->getIsActive()) {
+            if ($user->getIsConnected()) {
+                $user->markAsDisconnected();
+                $this->repository->save(entity: $user);
+            }
+            $event->markAsInvalid();
+            return;
         }
     }
 }
